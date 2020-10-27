72057e61bd7da45506830cb110737076365d5322
Yusuke Kishishita <31816350+bigface0202@users.noreply.github.com>
2020-10-27 23:11:21 +0900

    Add tutorial

--- lib/screens/training_screen.dart
+++ lib/screens/training_screen.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:tutorial_coach_mark/animated_focus_light.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import '../widgets/training_list.dart';
 import '../models/volume_prov.dart';
 import '../screens/new_training_screen.dart';
 
-class TrainingScreen extends StatelessWidget {
+class TrainingScreen extends StatefulWidget {
+  @override
+  _TrainingScreenState createState() => _TrainingScreenState();
+}
+
+class _TrainingScreenState extends State<TrainingScreen> {
+  List<TargetFocus> targets = List();
+  GlobalKey lockTutorialKey = GlobalKey();
+  TutorialCoachMark tutorialCoachMark;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    targets.add(
+      TargetFocus(
+        identify: 'test1',
+        keyTarget: lockTutorialKey,
+        contents: [
+          ContentTarget(
+            align: AlignContent.bottom,
+            child: Text('AAA'),
+          )
+        ],
+        shape: ShapeLightFocus.Circle,
+      ),
+    );
+    super.initState();
+  }
+
+  void showTutorial() {
+    tutorialCoachMark = TutorialCoachMark(context,
+        targets: targets,
+        colorShadow: Colors.red,
+        textSkip: "SKIP",
+        paddingFocus: 10,
+        opacityShadow: 0.8, onFinish: () {
+      print("finish");
+    }, onClickTarget: (target) {
+      print(target);
+    }, onClickSkip: () {
+      print("skip");
+    })
+      ..show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +75,7 @@ class TrainingScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        key: lockTutorialKey,
         child: Icon(Icons.add),
         onPressed: () async {
           await Navigator.of(context).pushNamed(NewTrainingScreen.routeName,
